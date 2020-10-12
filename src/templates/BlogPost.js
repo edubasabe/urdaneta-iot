1027c36cd046b5b615d3519c4859666b242bd97a
David Basabe <david.basabe@cencosud.cl>
2020-10-12 15:37:12 -0300

    :bug: Fix double import

--- src/templates/BlogPost.js
+++ src/templates/BlogPost.js
@@ -3,7 +3,7 @@ import { graphql } from 'gatsby'
 import Layout from '../components/layout'
 import SEO from '../components/seo'
 import { Heading, Link } from '@chakra-ui/core'
-import { Link as GatsbyLink, graphql } from 'gatsby'
+import { Link as GatsbyLink } from 'gatsby'
 
 const BlogPostTemplate = ({ data }) => (
   <Layout>
